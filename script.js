8aa78d3891f587e57627432accab041eda738354
Daniel Hawn <danieljhawn@gmail.com>
2020-09-17 21:28:59 -0500

    wordList added

---
+++ script.js
@@ -0,0 +1,27 @@
+// DATAMUSE API STUFF
+// const api = {
+//   url: "api.datamuse.com/words/",
+// }
+
+// document.getElementById("generate").addEventListener("click", generateName)
+
+// function generateName(params) {
+//   fetch(`${api.url}`)
+// }
+
+var noun = document.getElementById("noun");
+var verb = document.getElementById("verb");
+var adjective = document.getElementById("adjective");
+
+let wordTypes = []
+
+function randomNoun(){
+  nouns[Math.floor(Math.random()*nouns.length)];
+}
+
+function getCheckboxState() {
+  if (noun == true) {
+    wordTypes.push()
+  }
+
+}
