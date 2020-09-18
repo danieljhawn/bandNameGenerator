bbbb52ecd573da4081d4836650672d95fe31ac25
Daniel Hawn <danieljhawn@gmail.com>
2020-09-17 22:42:41 -0500

    MVP

--- script.js
+++ script.js
@@ -13,15 +13,35 @@ var noun = document.getElementById("noun");
 var verb = document.getElementById("verb");
 var adjective = document.getElementById("adjective");
 
-let wordTypes = []
-
-function randomNoun(){
-  nouns[Math.floor(Math.random()*nouns.length)];
+function randomAdjective() {
+  return adjectives[Math.floor(Math.random() * adjectives.length)];
 }
+function randomNoun() {
+  return nouns[Math.floor(Math.random() * nouns.length)];
+}
+// var randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+// var randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
 
-function getCheckboxState() {
-  if (noun == true) {
-    wordTypes.push()
-  }
+// var bandName = "Band Name"
 
+document.getElementById("generate").addEventListener("click", generateName)
+
+// function getCheckboxState() {
+//   if (noun.checked == true && adjectives.checked == true)  {
+//     bandName.replace("Band", randomAdjective)
+//     bandName.replace("Name", randomNoun)
+//   } else if (noun.checked == true && adjective.checked == false) {
+//     bandName.replace("Band", randomNoun)
+//     bandName.replace("Name", "")
+//   } else if (adjective.checked == true && noun.checked == false) {
+//     bandName.replace("Band", "")
+//     bandName.replace("Name", "randomAdjective")
+//   }
+
+//   console.log(bandName);
+// }
+
+function generateName() {
+  document.getElementById("first-word").innerText = randomAdjective();  
+  document.getElementById("second-word").innerText = randomNoun();  
 }
